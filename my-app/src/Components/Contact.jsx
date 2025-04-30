e531cf86eabff8ea0bf7a3cd710fd9a3c972e8c7
Tania Moura <moura4design@gmail.com>
2025-04-30 16:44:36 +0100

    update contact

--- my-app/src/Components/Contact.jsx
+++ my-app/src/Components/Contact.jsx
@@ -11,11 +11,16 @@ const Contact = () => {
           <div className='card-header text-center'>You can contact me</div>
           <div className='card-body'>
             <p>You can download my CV below.</p>
-            <button type="button" className='btn btn-sm mb-3 btn-color'>Download</button>
+            <a href="/C.V.pdf" download className="btn btn-sm mb-3 btn-color">
+              Download
+            </a>
+            {/* <button type="button" className='btn btn-sm mb-3 btn-color'>Download</button> */}
             <p><i className="bi bi-envelope-fill me-2" />Email: <a className='nav-link link-hover' href='malito:moura4design@gmail.com'>moura4design@gmail.com</a></p>
             <p><b>On in internet:</b></p>
             <p><i className="bi bi-linkedin me-2" /><a className='nav-link link-hover' target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/tania-moura/'>linkedin</a></p>
             <p><i className="bi bi-github me-2" /><a className='nav-link link-hover' target="_blank" rel="noopener noreferrer" href='https://github.com/Moura4Design'>github</a></p>
+            <p><a className='nav-link link-hover' target="_blank" rel="noopener noreferrer" href='https://bitbucket.org/TaniaMoura/workspace/repositories/'>bitbucket</a></p>
+            <p><i className="bi bi-behance me-2" /><a className='nav-link link-hover' target="_blank" rel="noopener noreferrer" href='https://www.behance.net/Moura_design'>behance</a></p>
           </div>
         </div>
       </div>
